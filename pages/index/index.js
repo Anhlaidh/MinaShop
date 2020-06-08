3b655dfd726b0c05185fb82427ed51b77a2f31d4
Anhlaidh <chinahantianzhao@foxmail.com>
2020-06-08 18:14:12 +0800

    finish index,lists,category

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,7 @@
-//Page Object
+//0 引入 用来发送请求的 方法 一定要把路径补充全
+import {
+  request
+} from "../../request/index.js";
 Page({
   data: {
     //轮播图数组
@@ -9,44 +12,54 @@ Page({
     floorList: []
 
   },
-  //options(Object)
+  //页面加载就会触发事件
   onLoad: function (options) {
-    wx.request({
-      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
-      success: (result) => {
+    //1 发送异步请求获取轮播图数据  优化的手段可以通过es6的 promise来解决这个问题 
+    // wx.request({
+    //   url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+    //   success: (result) => {
+    //     this.setData({
+    //       swiperList: result.data.message
+    //     })
+    //   }
+    // });
+
+    this.getSwiperList();
+    this.getCateList();
+    this.getFloorList();
+  },
+  // 获取轮播图数据
+  getSwiperList() {
+    request({
+        url: "/home/swiperdata"
+      })
+      .then(result => {
         this.setData({
-          swiperList: result.data.message
+          swiperList: result
         })
-      }
-    });
-
+      })
   },
-  onReady: function () {
-
+  // 获取 分类导航数据
+  getCateList() {
+    request({
+        url: "/home/catitems"
+      })
+      .then(result => {
+        this.setData({
+          catesList: result
+        })
+      })
   },
-  onShow: function () {
-
+  // 获取 楼层数据
+  getFloorList() {
+    request({
+        url: "/home/floordata"
+      })
+      .then(result => {
+        this.setData({
+          floorList: result
+        })
+      })
   },
-  onHide: function () {
 
-  },
-  onUnload: function () {
-
-  },
-  onPullDownRefresh: function () {
-
-  },
-  onReachBottom: function () {
-
-  },
-  onShareAppMessage: function () {
-
-  },
-  onPageScroll: function () {
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap: function (item) {
-
-  }
 });
